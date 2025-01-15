12c4000db062e5203a2cc1956474b5232bf11a60
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-16 03:34:01 +0400

    add styles for main header file

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  Language,
+  LanguageContainer,
+  MainHeaderContainer,
+  SaleInfo,
+  ShopNow,
+} from "./MainHeaderStyles";
+import Arrow from "/drop-down.png";
 
 const MainHeader: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <MainHeaderContainer>
+        <SaleInfo>
+          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+        </SaleInfo>
+        <ShopNow>ShopNow</ShopNow>
+        <LanguageContainer>
+          <Language>English</Language>
+          <img src={Arrow} alt="Arrow" />
+        </LanguageContainer>
+      </MainHeaderContainer>
+    </>
+  );
 };
 
 export default MainHeader;
---
+++ src/components/MainHeaderStyles.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const MainHeaderContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 4.8rem;
+  padding: 1.2rem 13.6rem 1.2rem 44.5rem;
+  background-color: #000000;
+`;
+
+export const SaleInfo = styled.span`
+  font-size: 1.4rem;
+  font-weight: normal;
+  line-height: 1.5;
+  color: #fafafa;
+`;
+
+export const LanguageContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 23.1rem;
+`;
+
+export const ShopNow = styled.span`
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: 1.71;
+  color: #fafafa;
+  margin-left: 0.4rem;
+`;
+
+export const Language = styled.span`
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 1.5;
+  color: #fafafa;
+`;
