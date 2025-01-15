d0c4943167cb3171835b2b11a5fa609c80ac6452
nk-on <2nkoz@proton.me>
2025-01-14 22:43:30 -0800

    Style:adding underLines to the menu items

--- src/components/Home/Menu.tsx
+++ src/components/Home/Menu.tsx
@@ -1,23 +1,41 @@
-export default function Menu(){
-    const menuItems = ["Home","Contact","About","Sign-up"];
-    return (
-        <div className="font-poppins flex justify-around items-center w-[100%] mt-[88px]">
-            <div>
-                <h1 className="font-inter font-bold text-[24px] tracking-[0.03em]">Exclusive</h1>
-            </div>
-            <div className="w-[50%]">
-                <ul className="flex justify-around">
-                {
-                    menuItems.map((menuItem) => <li className="font-normal text-[16px]">{menuItem}</li>)
-                }
-                </ul>
-            </div>
-            <div>
-                <div className="w-[243px] h-[38px] bg-[#F5F5F5] flex justify-between items-center px-[4px]">
-                    <p className="text-[12px] text-[#000000]">What are you looking for?</p>
-                    <img src="src/assets/Vector (1).svg" className="w-[16px] h-[16px]"></img>
-                </div>
-            </div>
+import ListItem from "./ListItem";
+const menuItems = [
+    "Home",
+    "Contact",
+    "About",
+    "Sign-up",
+  ];
+export default function Menu() {
+  return (
+    <div className="font-poppins flex justify-around items-center w-[100%] mt-[88px]">
+      <div>
+        <h1 className="font-inter font-bold text-[24px] tracking-[0.03em]">
+          Exclusive
+        </h1>
+      </div>
+      <div className="w-[50%]">
+        <ul className="flex justify-around">
+          {menuItems.map(
+            (menuItem,index) => {
+              return (
+                <ListItem key={index}  itemName = {menuItem} />
+              );
+            }
+          )}
+        </ul>
+      </div>
+      <div>
+        <div className="w-[243px] h-[38px] bg-[#F5F5F5] flex justify-between items-center px-[4px]">
+          <p className="text-[12px] text-[#000000]">
+            What are you
+            looking for?
+          </p>
+          <img
+            src="src/assets/Vector (1).svg"
+            className="w-[16px] h-[16px]"
+          ></img>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
