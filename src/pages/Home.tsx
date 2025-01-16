bdf231e8d46e578d82387d3433aa98c6fbd5cc72
nk-on <2nkoz@proton.me>
2025-01-15 22:05:48 -0800

    Adding skeleton of ad banner and categories item

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,30 @@
-import Menu from "../components/Home/Menu"
-export default function Home(){
-    return (
-        <>
-          <Menu />
-        </>
-    )
+import Menu from "../components/Home/Menu";
+export default function Home() {
+  const ListItems = [
+    "Woman’s Fashion",
+    "Men’s Fashion",
+    "Home & Lifestyle",
+    "Medicine",
+    "Sports & Outdoor",
+    "Groceries & Pets",
+    "Health & Beauty",
+  ];
+  return (
+    <>
+      <Menu />
+      <div className="flex justify-around  items-center w-[90%] h-[344px] mt-[50px]">
+        <ul className="flex flex-col justify-between h-[100%]">
+          {ListItems.map((ListItem)=>{
+            return <div>
+              <li>{ListItem}</li>
+              <div></div>
+            </div>
+          })}
+        </ul>
+        <div>
+          <img src="src/assets/Frame 560.svg"></img>
+        </div>
+      </div>
+    </>
+  );
 }
