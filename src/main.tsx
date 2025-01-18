a84789ffc1ac191a25a5e063fe4e5d9647df3501
nk-on <2nkoz@proton.me>
2025-01-18 12:10:49 -0800

    Configuring routing of the Error page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter,Routes,Route } from 'react-router'
 import Home from './pages/Home.tsx'
+import ErrorPage from './components/Home/ErrorPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
      <BrowserRouter>
        <Routes>
            <Route path='/' element = {<Home />}></Route>
+           <Route path='*' element = {<ErrorPage />}></Route>
        </Routes>
      </BrowserRouter>
   </StrictMode>,
