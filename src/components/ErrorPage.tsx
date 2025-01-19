87298f9434f7fe6aa6f74ae9c397023c9c1ace74
nk-on <2nkoz@proton.me>
2025-01-18 22:01:07 -0800

    Refactor:moving Go back section as a separete component

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,13 +1,9 @@
-import { Link } from "react-router";
+import GoBack from "./GoBack";
 
 export default function ErrorPage() {
   return (
     <>
-      <div className="flex font-poppins p-[50px] gap-[20px]">
-        <Link to={'/'}><p className="text-[#D3D3D3]">Home</p></Link>
-        <div>/</div>
-        <p>404 Error</p>
-      </div>
+      <GoBack componentName= {"404 Not Found"}/>
       <div className="w-[100%] h-[70vh] flex justify-center items-center">
       <div className="flex flex-col items-center gap-[50px]">
         <h1 className="font-inter text-[#000000] text-[115px] tracking-[0.03em]">
