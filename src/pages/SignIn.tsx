f6765e65fd0f1f3e7089689f49d0aae00809ec9c
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-21 13:40:53 +0400

    add structure for Sign in page

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,25 @@
+import ProductImage from "/package.jpg";
+
 const SignIn: React.FC = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          <img src={ProductImage} alt="Product image" />
+        </div>
+        <div>
+          <h4>Log in to Exclusive</h4>
+          <p>Enter your details below</p>
+          <form>
+            <input type="email" />
+            <input type="password" />
+            <div>
+              <button type="submit">Log In</button>
+              <span>Forget Password?</span>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
