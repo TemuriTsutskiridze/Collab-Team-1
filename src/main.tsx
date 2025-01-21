1c145fcdb19c32c72368027eaabd03bf2a1e223d
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-21 13:44:46 +0400

    add SigIn.tsx file in routing system

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import MainLayout from "./layouts/MainLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "/SignUp", element: <SignUp /> }],
+    children: [
+      { path: "/SignUp", element: <SignUp /> },
+      { path: "SignIn", element: <SignIn /> },
+    ],
   },
 ]);
 
