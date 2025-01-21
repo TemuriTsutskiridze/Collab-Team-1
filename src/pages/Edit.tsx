f6a3b83a972520c1608f557d4c62bc1d61ae7691
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-21 16:35:05 +0400

    add structure for the changing data field

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -1,7 +1,42 @@
 const Edit: React.FC = () => {
   return (
     <>
-      <div></div>
+      <aside></aside>
+      <div>
+        <h4>Edit Your Profile</h4>
+        <form>
+          <div>
+            <div>
+              <label>First Name</label>
+              <input />
+            </div>
+            <div>
+              <label>Last Name</label>
+              <input />
+            </div>
+          </div>
+          <div>
+            <div>
+              <label>Emmail</label>
+              <input />
+            </div>
+            <div>
+              <label>Address</label>
+              <input />
+            </div>
+          </div>
+          <div>
+            <label>Password Changes</label>
+            <input type="text" placeholder="Current Password" />
+            <input type="text" placeholder="New Password" />
+            <input type="text" placeholder="Confirm Password" />
+          </div>
+          <div>
+            <div>Cancel</div>
+            <button type="submit">Save Changes</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
